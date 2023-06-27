a9cb2862685a3dc711e7bd59a0ad96b2d94816e8
Ikenna <iykee0333@gmail.com>
2023-06-27 14:50:24 +0100

    added data file

---
+++ src/utils/data.ts
@@ -0,0 +1,99 @@
+export const cardsData = [
+	{
+		id: 1,
+		title: "Project Plan",
+		content:
+			"Ever and strange objects passed sister heralds crime his to to, some in made one seraphs, pile youth that now so he not. He holy the run parting apart to.",
+	},
+	{
+		id: 2,
+		title: "Interior Work",
+		content:
+			"Of and through drowsy holy childe. Times passion that what woe mother to but heralds with, things had deemed had his, pile by shrine before a another flatterers formed.",
+	},
+	{
+		id: 3,
+		title: "Realization",
+		content:
+			"Pilgrimage ere to so pillared seemed grief nor would, but time had lay it not, oh high fall would lyres domestic mood, knew in would hope venerable. Are my her.",
+	},
+];
+
+export const testimonials = [
+	{
+		id: 1,
+		image: "/person_1.png",
+		name: "Amelia James",
+		location: "California, USA",
+		text: "For in to to but she. Now birth so are this vile. For things to that and agen the baseless flaunting.",
+	},
+	{
+		id: 2,
+		image: "/person_2.png",
+		name: "Rick James",
+		location: "California, USA",
+		text: "Open steel his hall from almost eros he to. Talethis did heal men ancient congealed hight him him come, done.",
+	},
+	{
+		id: 3,
+		image: "/person_3.png",
+		name: "Ginny Greene",
+		location: "Georgia, USA",
+		text: "Heralds time times sick fondly be bidding delight. Childe of not sighed delphis the there, her spoiled domestic come but loved to.",
+	},
+];
+
+export const articles = [
+	{
+		id: 1,
+		image: "/kitchen_2.jpg",
+		title: "Shore laden still separate stock deep lenore, now.",
+		tag: "Kitchen Design",
+		date: "16 August, 2022",
+	},
+	{
+		id: 2,
+		image: "/white_room.jpg",
+		title: "Shore laden still separate stock deep lenore, now.",
+		tag: "Bedroom Design",
+		date: "10 February, 2023",
+	},
+	{
+		id: 3,
+		image: "/dining.jpg",
+		title: "Shore laden still separate stock deep lenore, now.",
+		tag: "Dining Design",
+		date: "20 April, 2023",
+	},
+];
+
+export const teamData = [
+	{
+		id: 1,
+		name: "Charles Stinson",
+		image: "/charles.jpg",
+		email: "charlestinson@mail.com",
+		phone: "+1 (378) 400-2764",
+	},
+	{
+		id: 2,
+		name: "Adanna Obi",
+		image: "/adanna.jpg",
+		email: "adanna_o@mail.com",
+		phone: "+1 (378) 360-2934",
+	},
+	{
+		id: 3,
+		name: "Hannah Cruz",
+		image: "/hannah.jpg",
+		email: "hannahcruz@mail.com",
+		phone: "+1 (378) 420-2747",
+	},
+	{
+		id: 4,
+		name: "Courtney Cook",
+		image: "/courtney.jpg",
+		email: "courtneycook@mail.com",
+		phone: "+1 (378) 400-9375",
+	},
+];
